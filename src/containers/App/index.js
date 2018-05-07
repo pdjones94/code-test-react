f9e7005de09895f34a8d8ba677d79358eb7bd18c
Marcin Wieprzkowicz <marcin.wieprzkowicz@gmail.com>
2018-05-07 16:12:48 +0200

    Rename about to checkout

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from '../Home';
-import About from '../About';
+import Checkout from '../Checkout';
 
 const App = () => (
   <div>
     <header>
       <Link to="/">Home</Link>
-      <Link to="/about-us">About</Link>
+      <Link to="/checkout">Checkout</Link>
     </header>
 
     <main>
       <Route exact path="/" component={Home} />
-      <Route exact path="/about-us" component={About} />
+      <Route exact path="/checkout" component={Checkout} />
     </main>
   </div>
 );
